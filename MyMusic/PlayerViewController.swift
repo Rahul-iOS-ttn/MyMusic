b95f73307a58b1b8758681493c9de130c836f34c
Rahul Sharma <rahul.sharma1@tothenew.com>
2021-12-05 14:08:34 +0530

    Initial Commit

---
+++ MyMusic/PlayerViewController.swift
@@ -0,0 +1,253 @@
+//
+//  PlayerViewController.swift
+//  MyMusic
+//
+//  Created by TTN on 03/12/21.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayerViewController: UIViewController {
+    
+    public var position: Int = 0
+    public var songs: [Song] = []
+    
+    @IBOutlet var holderView: UIView!
+    
+    var player: AVAudioPlayer?
+    
+    // user interface elements
+    
+    ///Also this pattern to create this is called anonymous closure
+    private let albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
+    
+    let playPauseButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if holderView.subviews.count == 0 {
+            configure()
+        }
+    }
+    
+    func configure() {
+        
+        //MARK:- Player
+        let song = songs[position]
+        
+        let urlString = Bundle.main.path(forResource: song.trackName, ofType: "mp3")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let urlString = urlString else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                return
+            }
+            
+            player.volume = 0.5
+            
+            player.play()
+            
+        } catch {
+            print("Error Occured")
+        }
+        
+        //MARK:- User Interface Elements
+        
+        // album cover
+        albumImageView.frame = CGRect(x: 10,
+                                      y: 10,
+                                      width: holderView.frame.size.width - 20,
+                                      height: holderView.frame.size.width - 20)
+        
+        albumImageView.image = UIImage(named: song.songCover)
+        holderView.addSubview(albumImageView)
+        
+        // labels: order - song name, album, artist
+        songNameLabel.frame = CGRect(x: 10,
+                                     y: albumImageView.frame.size.height + 10,
+                                     width: holderView.frame.size.width - 20,
+                                     height: 70)
+        albumNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height + 10 + 70,
+                                      width: holderView.frame.size.width - 20,
+                                      height: 70)
+        artistNameLabel.frame = CGRect(x: 10,
+                                       y: albumImageView.frame.size.height + 10 + 140,
+                                       width: holderView.frame.size.width - 20,
+                                       height: 70)
+        
+        songNameLabel.text = song.songName
+        albumNameLabel.text = song.albumName
+        artistNameLabel.text = song.artistName
+        
+        holderView.addSubview(songNameLabel)
+        holderView.addSubview(albumNameLabel)
+        holderView.addSubview(artistNameLabel)
+        
+        //player controls
+        
+        
+        let nextButton =  UIButton()
+        let backButton =  UIButton()
+        
+        // Button Frames
+        
+        let yPosition = artistNameLabel.frame.origin.y + 70 + 20
+        let size: CGFloat = 70
+        
+        playPauseButton.frame = CGRect(x: (holderView.frame.size.width - size) / 2.0,
+                                       y: yPosition,
+                                       width: size,
+                                       height: size)
+        nextButton.frame = CGRect(x: holderView.frame.size.width - size - 20,
+                                  y: yPosition,
+                                  width: size,
+                                  height: size)
+        backButton.frame = CGRect(x: 20,
+                                  y: yPosition,
+                                  width: size,
+                                  height: size)
+        
+        // Add Actions
+        playPauseButton.addTarget(self, action: #selector(didTapPlayPauseButton), for: .touchUpInside)
+        backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+        
+        // Button Styling
+        playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
+        backButton.setBackgroundImage(UIImage(systemName: "backward.fill"), for: .normal)
+        nextButton.setBackgroundImage(UIImage(systemName: "forward.fill"), for: .normal)
+        
+        playPauseButton.tintColor = .black
+        backButton.tintColor = .black
+        nextButton.tintColor = .black
+        
+        holderView.addSubview(playPauseButton)
+        holderView.addSubview(nextButton)
+        holderView.addSubview(backButton)
+        
+        //slider
+        let slider = UISlider(
+            frame: CGRect(x: 20,
+                          y: holderView.frame.size.height - 60,
+                          width: holderView.frame.size.height - 40,
+                          height: 50
+            ))
+        slider.value = 0.5
+        slider.addTarget(self, action: #selector(didSlideSlider(_:)), for: .valueChanged)
+        
+        holderView.addSubview(slider)
+    }
+    
+    @objc func didTapBackButton() {
+        if position > 0{
+            position -= 1
+            player?.stop()
+            
+            for subview in holderView.subviews {
+                subview.removeFromSuperview()
+            }
+            configure()
+        }
+    }
+    
+    @objc func didTapNextButton() {
+        if position < (songs.count - 1) {
+            position += 1
+            player?.stop()
+            
+            for subview in holderView.subviews {
+                subview.removeFromSuperview()
+            }
+            configure()
+        }
+    }
+    
+    @objc func didTapPlayPauseButton() {
+        if player?.isPlaying == true {
+            player?.pause()
+            
+            // show play button
+            playPauseButton.setBackgroundImage(UIImage(systemName: "play.fill"), for: .normal)
+            
+            // shrink image on pause
+            UIView.animate(withDuration: 0.2, animations: {
+                self.albumImageView.frame = CGRect(x: 30,
+                                                   y: 30,
+                                                   width: self.holderView.frame.size.width - 60,
+                                                   height: self.holderView.frame.size.width - 60)
+            })
+            
+        } else {
+            player?.play()
+            
+            playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
+            
+            // increase image size
+            UIView.animate(withDuration: 0.2, animations: {
+                self.albumImageView.frame = CGRect(x: 10,
+                                                   y: 10,
+                                                   width: self.holderView.frame.size.width - 20,
+                                                   height: self.holderView.frame.size.width - 20)
+            })
+        }
+    }
+    
+    @objc func didSlideSlider(_ slider: UISlider) {
+        let value = slider.value
+        
+        //adjust player audio
+        
+        player?.volume = value
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let player = player {
+            player.stop()
+        }
+    }
+}
---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by TTN on 03/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var songs = [Song]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSongs()
+        table.delegate = self
+        table.dataSource = self
+    }
+    
+    //MARK: - Song Configuration
+    func configureSongs() {
+        songs.append(Song(songName: "Background music", albumName: "something", artistName: "Rnado", songCover: "cover1", trackName: "song1"))
+        songs.append(Song(songName: "Havana", albumName: "Havana album", artistName: "camilla cabello", songCover: "cover2", trackName: "song2"))
+        songs.append(Song(songName: "Viva La Vida", albumName: "Dont know seriously", artistName: "cold play", songCover: "cover3", trackName: "song3"))
+        
+        // duplicated for population
+        songs.append(Song(songName: "Background music", albumName: "something", artistName: "Rnado", songCover: "cover1", trackName: "song1"))
+        songs.append(Song(songName: "Havana", albumName: "Havana album", artistName: "camilla cabello", songCover: "cover2", trackName: "song2"))
+        songs.append(Song(songName: "Viva La Vida", albumName: "Dont know seriously", artistName: "cold play", songCover: "cover3", trackName: "song3"))
+        songs.append(Song(songName: "Background music", albumName: "something", artistName: "Rnado", songCover: "cover1", trackName: "song1"))
+        songs.append(Song(songName: "Havana", albumName: "Havana album", artistName: "camilla cabello", songCover: "cover2", trackName: "song2"))
+        songs.append(Song(songName: "Viva La Vida", albumName: "Dont know seriously", artistName: "cold play", songCover: "cover3", trackName: "song3"))
+        songs.append(Song(songName: "Background music", albumName: "something", artistName: "Rnado", songCover: "cover1", trackName: "song1"))
+        songs.append(Song(songName: "Havana", albumName: "Havana album", artistName: "camilla cabello", songCover: "cover2", trackName: "song2"))
+        songs.append(Song(songName: "Viva La Vida", albumName: "Dont know seriously", artistName: "cold play", songCover: "cover3", trackName: "song3"))
+    }
+    
+    //MARK:- Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let song = songs[indexPath.row]
+        
+        
+        //Cell Configuration
+        cell.textLabel?.text = song.songName
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: song.songCover)
+        
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        table.deselectRow(at: indexPath, animated: true)
+        
+        //Present the player
+        /// for this task we usually need an array of the songs/media and also the positioning from where we want to play the song/media
+        let position = indexPath.row
+        
+        guard let vc = storyboard?.instantiateViewController(identifier: "player") as? PlayerViewController else {
+            return
+        }
+        
+        vc.songs = songs
+        vc.position = position
+        
+        present(vc, animated: true)
+    }
+    
+}
+
+
+struct Song {
+    let songName: String
+    let albumName: String
+    let artistName: String
+    let songCover: String
+    let trackName: String // this will be the actual audio
+}
